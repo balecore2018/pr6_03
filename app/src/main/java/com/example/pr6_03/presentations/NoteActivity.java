93cef7b383ed478564a7207849730a7786a59a98
dima <balecored@gmail.com>
2026-03-10 13:06:12 +0500

    Программирование активности NoteActivity. шаповалкин

--- app/src/main/java/com/example/pr6_03/presentations/NoteActivity.java
+++ app/src/main/java/com/example/pr6_03/presentations/NoteActivity.java
@@ -1,6 +1,10 @@
 package com.example.pr6_03.presentations;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,71 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.pr6_03.R;
+import com.example.pr6_03.datas.RepoNotes;
+import com.example.pr6_03.domains.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NoteActivity extends AppCompatActivity {
+
+    Note note;
+    EditText etTitle, etText;
+    TextView tvDate;
+    View bthSelectColor, bthBack, bthTrash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_note);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Date DateNow = new Date();
+        SimpleDateFormat FormatForDateNow = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+
+        bthSelectColor = findViewById(R.id.bth_select_color);
+        bthBack = findViewById(R.id.bth_back);
+        bthTrash = findViewById(R.id.bth_trash);
+        etTitle = findViewById(R.id.et_title);
+        etText = findViewById(R.id.et_text);
+        tvDate = findViewById(R.id.tv_date);
+
+        Bundle arguments = getIntent().getExtras();
+        if(arguments != null){
+            int Position = arguments.getInt("position");
+            note = RepoNotes.Notes.get(Position);
+            etTitle.setText(note.title);
+            etText.setText(note.text);
+        } else {
+            bthTrash.setVisibility(View.GONE);
+        }
+        tvDate.setText("Отредактированно: " + FormatForDateNow.format(DateNow));
+
+        bthSelectColor.setOnClickListener(v -> {
+            String Title = etTitle.getText().toString();
+            String Text = etText.getText().toString();
+
+            if(Text
+                    .replace(" ", "")
+                    .replace("\r", "")
+                    .replace("\n", "")
+                    .isEmpty()){
+                Toast.makeText(this, "Не чего сохранять", Toast.LENGTH_SHORT).show();
+            } else {
+                if(note == null){
+                    note = new Note();
+                    RepoNotes.Notes.add(note);
+                }
+                note.title = Title;
+                note.text = Text;
+                note.date = FormatForDateNow.format(DateNow);
+            }
+            finish();
+        });
+
+        bthTrash.setOnClickListener(v -> {
+            RepoNotes.Notes.remove(note);
+            finish();
+            Toast.makeText(this,"Заметка удалена", Toast.LENGTH_SHORT).show();
         });
     }
 }
